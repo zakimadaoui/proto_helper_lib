9d3a055ae35a93dc883e64a20b0c1bc5bbcd0bc8
zaki <zaki@zaki-hp.localdomain>
2021-09-18 11:01:45 +0100

    fix examples

--- examples/dimmer_demo/dimmer_demo.ino
+++ examples/dimmer_demo/dimmer_demo.ino
@@ -5,35 +5,32 @@
 ProtoHelper &phelper = ProtoHelper::getInstance();   //init the helper
 
 // a callback function that triggers when a command is fully recieved
-void onRecieve(Command *command) 
-{
+void onRecieve(Command *command){
     // The GUI has a Knob widget that sends the command :
     // "> DIMM, integer(intensity)" and 'intensity' varries from 0 to 255
     
-    if (strcmp("DIMM", command->getCommandName()))
-    {
+    if (strcmp("DIMM", command->getCommandName()) == 0){
         int intensity = command->getParam(0)->getIntValue();
         analogWrite(PWM_PIN, intensity);
     }
 
     // OR, to avoid using string comparision use getNameAsChar() 
     // with commandName='D' instead of 'DIMM'(Best Practice)
-
-    else if (command->getNameAsChar() == 'D')       // the commandName 'D' stands for DIMM
+    /*
+    if (command->getNameAsChar() == 'D')       // the commandName 'D' stands for DIMM
     {
         int intensity = command->getParam(0)->getIntValue();
         analogWrite(PWM_PIN, intensity);
     }
+    */
 }
 
-void setup()
-{
+void setup(){
     phelper.setOnCommandRecievedCallabck(onRecieve); //the OnRecieve callback
     Serial.begin(9600);                              //set baudrate to 9600
 }
 
-void loop()
-{
+void loop(){
     while (Serial.available())
         phelper.loadByte(Serial.read());            //load all bytes from serial bus
 }
--- examples/two_dimmers/two_dimmers.ino
+++ examples/two_dimmers/two_dimmers.ino
@@ -6,36 +6,31 @@
 ProtoHelper &phelper = ProtoHelper::getInstance();   //init the helper
 
 // a callback function that triggers when a command is fully recieved
-void onRecieve(Command *command) 
-{
+void onRecieve(Command *command) {
 
     char whichLED = command->getParam(0)->getCharValue();
     int intensity = command->getParam(1)->getIntValue();
 
     // using a character instead of a full string as commandName is the best Practice
-    if (command->getNameAsChar() == 'D')       
-    {   
-        switch (whichLED)
-        {
-        case '1':
+    if (command->getNameAsChar() == 'D'){
+        switch (whichLED){
+            case '1':
             analogWrite(PWM_PIN_1, intensity);
             break;
         
-        case '2':
+            case '2':
             analogWrite(PWM_PIN_2, intensity);
             break;
         }
     }
 }
 
-void setup()
-{
+void setup(){
     phelper.setOnCommandRecievedCallabck(onRecieve); //the OnRecieve callback
     Serial.begin(9600);                              //set baudrate to 9600
 }
 
-void loop()
-{
+void loop(){
     while (Serial.available())
         phelper.loadByte(Serial.read());            //load all bytes from serial bus
 }
